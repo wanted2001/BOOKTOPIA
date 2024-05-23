25529612b7026fd4435f62de1075d62c09ee85a3
Ceciliaseon <mangseon2@gmail.com>
2024-05-23 12:11:31 +0900

    booktopia BoardVO create

---
+++ www/src/main/java/com/booktopia/www/domain/BoardVO.java
@@ -0,0 +1,10 @@
+package com.booktopia.www.domain;
+
+public class BoardVO {
+
+    private long bno;
+    private String id;
+    private String bTitle;
+    private String bWriter;
+    private String bContent;
+}
