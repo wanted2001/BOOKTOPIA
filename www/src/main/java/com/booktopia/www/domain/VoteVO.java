d31a30fafabbdd9685e3e1416766eec845c293c1
havanara <dlskfk1414@gmail.com>
2024-05-23 15:17:17 +0900

    정온_VO작업

---
+++ www/src/main/java/com/booktopia/www/domain/VoteVO.java
@@ -0,0 +1,16 @@
+package com.booktopia.www.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteVO {
+
+    private int voteBno; //찬반게시글번호(1)
+    private String id; //투표할 유저 아이디
+    private int voteResult; //투표결과
+
+}
