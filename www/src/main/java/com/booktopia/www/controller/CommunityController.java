3805f0e0607aece9f8febe8a6970989fb77a4d88
gajami-kim <qansd38@gmail.com>
2024-05-24 18:04:24 +0900

    정온 community js 작업

--- www/src/main/java/com/booktopia/www/controller/CommunityController.java
+++ www/src/main/java/com/booktopia/www/controller/CommunityController.java
@@ -1,19 +1,32 @@
 package com.booktopia.www.controller;
 
 import com.booktopia.www.service.CommunityService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/comm/*")
+@RequestMapping("/community/*")
 @Controller
+@RequiredArgsConstructor
 public class CommunityController {
 
     private final CommunityService csv;
 
+    @GetMapping("/communityMain")
+    public void commMain(){}
 
+    @GetMapping("/communityList")
+    public void commList(){}
+
+    @GetMapping("/communityDetail")
+    public void commDetail(){}
+
+    @GetMapping("/communityNotice")
+    public void commNotice(){}
 
 }
