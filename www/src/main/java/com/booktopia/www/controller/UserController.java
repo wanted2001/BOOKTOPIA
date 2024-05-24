62f3a4541a4399c1321eb8eb10e1d5a4f70608fb
wanted2001 <jimyungsla@naver.com>
2024-05-24 15:45:46 +0900

    지명-로그인 중간 점검

---
+++ www/src/main/java/com/booktopia/www/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.booktopia.www.controller;
+
+import com.booktopia.www.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/user/*")
+@Controller
+public class UserController {
+
+    private final UserService usv;
+
+    @GetMapping("/login")
+    public void login(){}
+
+    @GetMapping("/join")
+    public void join(){}
+}
