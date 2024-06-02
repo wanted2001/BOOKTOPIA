ec4ae815f4b885a2602b9356a6e702474369a730
wanted2001 <jimyungsla@naver.com>
2024-06-02 19:08:26 +0900

    로그인 정보 불러오기 OAUTH 재업로드

---
+++ www/src/main/java/com/booktopia/www/oauth2/service/CustomOAuth2UserService.java
@@ -0,0 +1,80 @@
+package com.booktopia.www.oauth2.service;
+
+import com.booktopia.www.domain.AuthVO;
+import com.booktopia.www.domain.UserVO;
+import com.booktopia.www.oauth2.exception.OAuth2AuthenticationProcessingException;
+import com.booktopia.www.oauth2.user.OAuth2UserInfo;
+import com.booktopia.www.oauth2.user.OAuth2UserInfoFactory;
+import com.booktopia.www.repository.UserMapper;
+import com.booktopia.www.security.AuthUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        try {
+            return processOAuth2User(userRequest, oAuth2User);
+        } catch (AuthenticationException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+        }
+    }
+    // OAuth에 들어가 정보를 있는 없는지 확인하는 메서드
+    private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String accessToken = userRequest.getAccessToken().getTokenValue();
+
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(registrationId, accessToken, oAuth2User.getAttributes());
+
+        // OAuth2UserInfo field value validation
+        if (!StringUtils.hasText(oAuth2UserInfo.getId())) {
+            throw new OAuth2AuthenticationProcessingException("OAuth2 no id");
+        }
+        if (!StringUtils.hasText(oAuth2UserInfo.getPwd())) {
+            throw new OAuth2AuthenticationProcessingException("OAuth2 no pwd");
+        }
+        if (!StringUtils.hasText(oAuth2UserInfo.getName())) {
+            throw new OAuth2AuthenticationProcessingException("OAuth2 no name");
+        }
+
+        UserVO userVO = userMapper.findByEmail(oAuth2UserInfo.getPwd());
+        if (userVO == null) {
+            userVO = new UserVO();
+            userVO.setId(oAuth2UserInfo.getId());
+            userVO.setPwd(oAuth2UserInfo.getPwd());
+            userVO.setName(oAuth2UserInfo.getName());
+            userVO.setUserType(oAuth2UserInfo.getUserType());
+            log.info("Mapper 직전 uvo={}", userVO);
+            // 여기에 추가적인 필드도 설정할 수 있습니다.
+            int isOk = userMapper.saveOauthUser(userVO);
+            if (isOk > 0) {
+                userMapper.insertAuth(userVO.getPwd());
+            } else {
+                log.info("DB에 못들어감");
+            }
+        }
+
+        return new OAuth2UserPrincipal(oAuth2UserInfo);
+    }
+}
