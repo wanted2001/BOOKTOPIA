624ee32d94f5153e3747be4c53e25736ebffcca6
gajami-kim <qansd38@gmail.com>
2024-06-10 09:37:17 +0900

    정온 게시판 기능 끝

--- www/src/main/java/com/booktopia/www/controller/CommunityController.java
+++ www/src/main/java/com/booktopia/www/controller/CommunityController.java
@@ -9,6 +9,7 @@ import com.booktopia.www.handler.FileHandler;
 import com.booktopia.www.handler.PagingHandler;
 import com.booktopia.www.service.BoardService;
 import com.booktopia.www.service.CommunityService;
+import com.booktopia.www.service.VoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -70,6 +71,29 @@ public class CommunityController {
         }
         return "1";
     }
+
+    @GetMapping("/communityListAll")
+    public String commListAll(Model m, PagingVO pgvo){
+        log.info("all pgvo>>>>{}",pgvo);
+
+        //전체 게시글 수
+        int totalCount = bsv.getTotalCount(pgvo);
+
+        PagingHandler ph = new PagingHandler(pgvo,totalCount);
+        log.info("all ph>>>>>{}",ph);
+
+        List<BoardVO> blist = bsv.getList(pgvo);
+        log.info("all blist>>>{}", blist);
+
+        int AllcateCtn = bsv.getCateCount();
+        log.info("AllcateCtn>>>{}",AllcateCtn);
+
+        m.addAttribute("blist",blist);
+        m.addAttribute("ph",ph);
+        m.addAttribute("AllcateCtn",AllcateCtn);
+        return "/community/communityList";
+    }
+
 
     @GetMapping("/communityList")
     public void commListCategory(Model m, PagingVO pgvo, @RequestParam("bCate")String bCate){
